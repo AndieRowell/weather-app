96e20e3fe5d001ee59ac87a82935c055fdad90e2
Andie <134252065+AndieRowell@users.noreply.github.com>
2023-09-28 10:10:17 -0400

    adding to js pseudocode day 2

--- js/main.js
+++ js/main.js
@@ -16,6 +16,8 @@
 // functions are used to take the data and populate it into the created elements
 //
 
+// built in api request by zip code - weather
+// https://openweathermap.org/current#zip
 
 //CONSIDER
 
@@ -25,3 +27,43 @@
 
 // the zipcode is a variable that we make
 // result is an object - get to some of those properties with dot notation
+
+//===========================================================================
+
+// DAY 2 FRESH EYES PSEUDOCODE:
+
+//===========================================================================
+
+// SETUP
+// html should have one div - add an id attribute to reference it later "weather-app"
+//      <div id="weather-app"></div>
+
+// init my variables
+// - variables
+//      - what do i need to have?
+//          - weather app to connect to the div
+//          - input
+//              - placeholder text
+//              -
+//          - submit button
+//          - display boxes
+//
+
+// - basically repeating creating elements that are similar to display data
+// - can use createElement?
+
+// ACTIONABLE ITEMS
+// - submit button needs a click event listener to call to the api
+//      - axios request to the weather api
+//      - use the given api key
+//      - the response you get should return the data into the corresponding variables that were initialized
+//      - display that data
+//      - populate the data into the "html" text displays (replace the placholders?)
+//      - clarify the 'catch' errors and populate the error text displays
+
+// still need to use appendChild to append the elements to the weather app div
+
+
+// EXTRAS - STRETCH GOALS?
+// - a toggle button to change from celsius to fahrenheit - vice versa
+//      - probably use an if else statement with the click handler?
